0ce14f5589ea512301b58e032d80378df24a31e6
siddhishree <siddhishree.shree5@gmail.com>
2017-11-25 01:52:08 +0530

    dashboard

--- home.php
+++ home.php
@@ -25,7 +25,7 @@
 	$user = $_SESSION['user']; //assigns user value
 	?>
 
-  <body>
+  <body style="background-image: url(images/job.jpg); background-size: cover">
     <header>
       <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a class="navbar-brand" href="#">Dreamjob</a>
@@ -35,17 +35,17 @@
 
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            <li class="nav-item ">
               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Settings</a>
+              <a class="nav-link" href="#">Recruiters</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Job Seekers</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Profile</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link fixed-right" href="#">Help</a>
+              <a class="nav-link fixed-right" href="login-signup.html">Login/Signup</a>
             </li>
           </ul>
         </div>
@@ -56,35 +56,78 @@
         <nav class="col-sm-3 col-md-2 d-none d-sm-block bg-light sidebar">
           <ul class="nav nav-pills flex-column">
             <li class="nav-item">
-            <a class="nav-link active" href="#"><?php echo "$user"?> <span class="sr-only">(current)</span></a>
+            <a class="nav-link active" href="#"><?php echo "$user"?><span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Search</a>
+              <a class="nav-link" href="#">Jobs Applied</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Resume</a>
             </li>
-          </ul>
-
-          <ul class="nav nav-pills flex-column">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Not Decided</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Not Decided</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Not Decided</a>
-            </li>
-            <li class="nav-item">
+               <li class="nav-item">
               <a class="nav-link" href="scripts/logout.php">Log Out</a>
             </li>
           </ul>
+
+          
 
         </nav>
 
         <main role="main" class="col-sm-9 ml-sm-auto col-md-10 pt-3">
           <h1>Dashboard</h1>
+            <h3>Job Search</h3>
+             <div style="font-size: 20px;">
+            <form>
+               
+                <input type="text" placeholder="Skills">
+                <select placeholder="Experience">
+                    <option value="" disabled selected> &nbsp;&nbsp;Experience&nbsp;&nbsp; </option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    </select>
+                <select >
+                    <option value="" disabled selected> &nbsp;&nbsp;Salary(In LPA)&nbsp;&nbsp; </option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                    </select> &nbsp;&nbsp;<br><br>
+                <a class="btn btn-lg btn-primary" href="#" role="button">Search</a>
+            </form></div>
 
         </main>
       </div>
